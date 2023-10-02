39d1c282b9b9ea77685269c2c455f12c41133a1c
Minewarpro <hugo.roche13@hotmail.com>
2023-10-03 01:02:31 +0200

    mise en place video

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+var video = document.getElementById("video");
+var video_1 = document.getElementById("video_1");
+var video_2 = document.getElementById("video_2");
+
+timeoutId = 0;
+lastvideo = "";
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -23,3 +30,39 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle('active');
     });
 });
+
+function playAndPause () {
+
+    if (video.paused){
+        video.play();
+    }
+    else {
+        video.pause();
+
+    }
+}
+
+function play(video){
+    video.play();
+    if (lastvideo === video){
+        clearTimeout(timeoutId);
+    }
+    lastvideo = video;
+}
+
+function pause(video, url){
+
+    video.pause();
+
+    timeoutId = setTimeout(function (){
+
+        if (video.paused){
+            video.setAttribute('src', url);
+        }
+        if (lastvideo.paused){
+            video.setAttribute('src', url);
+        }
+
+    }, 100 );
+
+}
